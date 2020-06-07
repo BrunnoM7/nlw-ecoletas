c950421129ced3c1c025d62bbe877355f5200362
Brunno Marchetti <brunno.marchetti@ibm.com>
2020-06-06 21:49:38 -0300

    application back-end

---
+++ server/src/database/migrations/01_create_items.ts
@@ -0,0 +1,16 @@
+import Knex from 'knex';
+
+// Create table
+export async function up(knex: Knex) {
+  return knex.schema.createTable('items', table => {
+    table.increments('id').primary();
+    table.string('image').notNullable();
+    table.string('title').notNullable();
+  })
+
+}
+
+// Roll back (delete the table)
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('items');
+}
